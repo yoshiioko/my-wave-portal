f3343c4580b63000d99f6c8b832adad54c08844d
yoshiioko <acardoza@pdx.edu>
2022-11-04 10:29:07 -0700

    Update contract to award some ETH when people wave at it.

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -27,6 +27,12 @@ contract WavePortal {
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
         emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint prizeAmount = 0.0001 ether;
+        require(prizeAmount <= address(this).balance, "Trying to withdraw more money than the contract has.");
+
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract");
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
